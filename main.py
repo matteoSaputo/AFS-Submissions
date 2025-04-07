4f21b2ac9f5e83a832dda2b7b04e1d3bc3f03d4a
matteoSaputo <matteosaputo1201@gmail.com>
2025-04-07 12:26:24 -0700

    Fixed bug for saving afs app in google drive

--- main.py
+++ main.py
@@ -6,30 +6,32 @@ import os
 import re
 import shutil
 
-def rewrite_pdf(input_path, output_path):
-    reader = PdfReader(input_path)
-    writer = PdfWriter()
-    writer.addpages(reader.pages)
-    writer.write(output_path)
-
 def main():
 
+    afs_source = "./data/Business Application.pdf"
+
     # Extract data from afs application
-    afs_data = extract_afs_data("Business Application.pdf")
+    afs_data = extract_afs_data(afs_source)
 
     # Sanitize business name to avoid accidentally making weird folders
     bus_name = re.sub(r'[\\/*?:"<>|]', "_", afs_data["Business Legal Name"])
-    
+
+    # Rename afs app with business name
+    os.rename(afs_source, f"./data/Business Application - {bus_name}.pdf")
+    afs_source = f"./data/Business Application - {bus_name}.pdf"
+
+    customer_folder = f'./test'
+
     customer_folder = f'G:\Shared drives\AFS Drive\Customer Info\Customer Info/{bus_name}'
 
     # Create the customer folder if it doesn't exist
     os.makedirs(customer_folder, exist_ok=True)
 
-    # Save afs app in new folder
-    rewrite_pdf("Business Application.pdf", f"{customer_folder}/Business Application - {bus_name}")
-
     # Fill and save NRS Application
     fill_nrs(afs_data, customer_folder)
+
+    # move afs app in new folder
+    shutil.move(afs_source, customer_folder)
 
 if __name__ == "__main__":
     main()
