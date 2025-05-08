376b11a743b113aea9e4afced0ef4413fc316ff4
matteoSaputo <matteosaputo1201@gmail.com>
2025-05-08 16:21:04 -0400

    Empty upload folder when handling new files

--- main.py
+++ main.py
@@ -187,7 +187,24 @@ class AFSApp:
         pdf_files = [f.strip('{}') for f in dropped_files]
         self.handle_files(pdf_files)
 
+    def clean_uploads_folder(self):
+        files_to_delete =[f for f in os.listdir(UPLOAD_DIR) if f != "keep.txt"]
+
+        if not files_to_delete:
+            return
+        
+        for file in files_to_delete:
+            file_path = os.path.join(UPLOAD_DIR, file)
+            try:
+                if os.path.isfile(file_path) or os.path.islink(file_path):
+                    os.unlink(file_path)
+                elif os.path.isdir(file_path):
+                    shutil.rmtree(file_path)
+            except Exception as e:
+                print(f"Failed to delete {file_path}. {e}")
+
     def handle_files(self, file_list):
+        self.clean_uploads_folder()
         self.uploaded_files =[]
 
         for original_path in file_list:
