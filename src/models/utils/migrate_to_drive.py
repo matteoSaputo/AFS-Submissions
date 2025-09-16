688ecdf80644138b6d8ee7d2e83303a55d8e4a0e
matteoSaputo <matteosaputo1201@gmail.com>
2025-09-16 15:18:25 -0400

    flatten all docs before moving to drive

--- src/models/utils/migrate_to_drive.py
+++ src/models/utils/migrate_to_drive.py
@@ -1,11 +1,14 @@
 import os
 import shutil
 
+from models.utils.flatten_pdf import flatten_pdf
+
 def migrate_to_drive(files, drive):
     # Move files from "files" list into folder "drive"
     for file in files:
         if not file or not os.path.exists(file):
             continue
+        flatten_pdf(file, file)
         new_path = os.path.join(drive, os.path.basename(file))
         if os.path.exists(new_path):
             os.unlink(new_path)
